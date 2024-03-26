be1fd774ba27b66d95b042740ff8dffd046831df
chwwy <162992055+chwwy@users.noreply.github.com>
2024-03-26 18:57:16 +0700

    no API used

--- src/components/Tab2.svelte
+++ src/components/Tab2.svelte
@@ -1,32 +1,11 @@
 <script lang="ts" context="module">
+    // Export the component using a different name
     export { default as Tab2Content } from '../components/Tab2.svelte';
-    import { fetchLatestVideoIframe } from '../util';
-
-    let latestVideoIframe: string | undefined;
-
-    fetchLatestVideoIframe('YOUR_API_KEY', 'YOUR_CHANNEL_ID')
-        .then((iframe: string) => { // Assuming latestVideoIframe is a string
-            // Assign the result of fetchLatestVideoIframe to latestVideoIframe
-            latestVideoIframe = iframe;
-            console.log(latestVideoIframe);
-        })
-        .catch((error: Error) => { // Specify the type of 'error' parameter explicitly
-            console.error('Error fetching latest video:', error);
-            
-            // If an error occurs, fallback to a default video
-            latestVideoIframe = `<iframe width="560" height="315" src="https://www.youtube.com/watch?v=NPDDPyezBM8" frameborder="0" allowfullscreen></iframe>`;
-        });
 </script>
 
-<div id="default-tab-content" class="py-2 px-8 overflow-auto">
-    <div id="default-tab-title" class="text-base text-pink-100">
-        catch my latest youtube video/live here 🐇:
-    </div>
-    <div id="default-tab-content" class="aspect-w-16 aspect-h-9 flex justify-center mt-2">
-        {#if latestVideoIframe}
-            {@html latestVideoIframe}
-        {:else}
-            <iframe width="560" height="315" src="https://www.youtube.com/watch?v=NPDDPyezBM8" frameborder="0" allowfullscreen></iframe>
-        {/if}
-    </div>
+<div class="flex flex-wrap justify-center py-2">
+    catch my latest youtube video here: 🤖
+</div>
+<div class="aspect-w-16 aspect-h-9 flex justify-center">
+    <iframe width="560" height="315" class="" src="https://www.youtube.com/embed/NPDDPyezBM8?si=wzYl5AvNp7EvQz9i&amp;controls=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
 </div>
