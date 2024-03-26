d3731f32af9c04a8a9201d9c708b3a3a73561f05
chwwy <162992055+chwwy@users.noreply.github.com>
2024-03-26 16:20:17 +0700

    scrollable

--- src/components/LeftFooter.svelte
+++ src/components/LeftFooter.svelte
@@ -4,10 +4,10 @@
 
 <div id="default-tab-content" class="py-4 px-4 hover:bg-pink-100/10 duration-150">
     <div id="default-tab-title" class="text-xl text-pink-100 mb-2 flex flex-row flex-wrap gap-2">
-        <div class="social-icon">
+        <div class="social-icon overflow-auto">
             <svg  xmlns="http://www.w3.org/2000/svg"  width="40"  height="40"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="1.5"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-brand-medium"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M4 4m0 2a2 2 0 0 1 2 -2h12a2 2 0 0 1 2 2v12a2 2 0 0 1 -2 2h-12a2 2 0 0 1 -2 -2z" /><path d="M8 9h1l3 3l3 -3h1" /><path d="M8 15l2 0" /><path d="M14 15l2 0" /><path d="M9 9l0 6" /><path d="M15 9l0 6" /></svg>
         </div>
-        <div class="social-content flex flex-col">
+        <div class="social-content flex flex-col overflow-auto">
             <div class="social-name text-base">
                 medium
             </div>
